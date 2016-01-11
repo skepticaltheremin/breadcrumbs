f645847f01dc3a99c8f6ba9c8028f637b9e8341f
Lex <lex.cooke@gmail.com>
2016-01-10 16:27:47 -0800

    comment out toggleFavorite references

--- app/components/Map.js
+++ app/components/Map.js
@@ -34,9 +34,9 @@ var Map = React.createClass({
 
   },
 
-  toggleFavorite(address){
-    this.props.onFavoriteToggle(address);
-  },
+  // toggleFavorite(address){
+  //   this.props.onFavoriteToggle(address);
+  // },
 
   addFavBreadCrumb(id, lat, lng, timestamp, details, infoWindow, location) {
     console.log(timestamp);
